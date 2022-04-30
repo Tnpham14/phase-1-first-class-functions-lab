e273856d4ecb7a145e5d7cd978abd8b548f8b0ef
Tony Pham <tnpham@valdosta.edu>
2022-04-30 15:43:28 -0400

    commit tree b533698713a7e27ed0c34c59d7ca763934ce5c5a

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers= array => array. slice(0, 2)
+
+const returnLastTwoDrivers= array => array. slice(array.length-2, array.length)
+
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(int) {
+    return function(fare){
+        return fare * int
+    }
+}
+
+const fareDoubler=function(fare){
+    return fare *2
+}
+
+const fareTripler=function(fare){
+    return fare * 3
+}
+ function selectDifferentDrivers(aNames, searchFunction){
+    return searchFunction(aNames);
+ }
+
+
+
+
